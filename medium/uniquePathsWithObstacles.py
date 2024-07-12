18c075f6ae2e145dc194b32d85b88a746d769e78
Jake <45899701+Kodoh@users.noreply.github.com>
2024-07-12 12:55:47 +0100

    Create uniquePathsWithObstacles.py

---
+++ medium/uniquePathsWithObstacles.py
@@ -0,0 +1,28 @@
+class Solution:
+    def uniquePathsWithObstacles(self, obstacleGrid: List[List[int]]) -> int:
+        if not obstacleGrid or obstacleGrid[0][0] == 1:
+            return 0
+        
+        COLS = len(obstacleGrid[0])
+        ROWS = len(obstacleGrid)
+        
+        dp = [[-1] * COLS for _ in range(ROWS)]
+        
+        def dfs(i, j):
+            if i < 0 or j < 0 or i >= ROWS or j >= COLS or obstacleGrid[i][j] == 1:
+                return 0
+            
+            if dp[i][j] != -1:
+                return dp[i][j]
+            
+            if i == ROWS - 1 and j == COLS - 1:
+                return 1
+            
+            right_paths = dfs(i, j + 1)
+            down_paths = dfs(i + 1, j)
+            
+            dp[i][j] = right_paths + down_paths
+            
+            return dp[i][j]
+        
+        return dfs(0, 0)
